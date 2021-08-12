00350ed9e01d419c2f5a2237fa95113df66c674d
laxmanGhoghol <ghoghollaxman@gmail.com>
2021-08-12 18:46:06 +0530

    added Post model

--- blogApp/blog/views.py
+++ blogApp/blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import request
-# Create your views here.
+from .models import Post
+
 
 def Home(request):
-    return render(request, 'blog/home.html')
+    posts = Post.objects.all()
+    context = {"posts": posts, 'title': "Home"}
+    return render(request, "blog/home.html", context)
+
 
 def About(request):
-    return render(request, 'blog/about.html')
+    return render(request, "blog/about.html")
+
