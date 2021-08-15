9c487b575f34c6498fcc49aaaca270cff3549884
laxmanGhoghol <ghoghollaxman@gmail.com>
2021-08-15 13:23:36 +0530

    added user registraion form in users app

--- blogApp/blogApp/settings.py
+++ blogApp/blogApp/settings.py
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
