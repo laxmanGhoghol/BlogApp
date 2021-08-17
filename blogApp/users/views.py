4e2f2b0b65ce162088a08cc80eaca6ee68240909
laxmanGhoghol <ghoghollaxman@gmail.com>
2021-08-17 18:26:50 +0530

    added user profile pic in posts, added user posts in profile

--- blogApp/users/views.py
+++ blogApp/users/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
-
+from blog.models import Post
 
 def Register(request):
     if request.method == "POST":
@@ -30,7 +30,7 @@ def Profile(request):
         )
 
         if u_form.is_valid() and p_form.is_valid():
-            u_form.save()
+            u_form.save() #save updated data
             p_form.save()
             messages.success(
                 request, f"Your Account has  been updated!"
@@ -40,6 +40,8 @@ def Profile(request):
         u_form = UserUpdateForm(instance=request.user)
         p_form = ProfileUpdateForm(instance=request.user.profile)
 
-    context = {"u_form": u_form, "p_form": p_form, "title": "Profile"}
+    cur_user = request.user #get current user
+    posts = Post.objects.filter(author = cur_user) #get blogs created by user
+    context = {"u_form": u_form, "p_form": p_form, "title": "Profile", 'posts': posts}
     return render(request, "users/profile.html", context)
 
